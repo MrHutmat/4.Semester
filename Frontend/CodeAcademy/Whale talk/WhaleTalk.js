57ebc5b3fd2d8bf653ba49169d1bafff088e7e86
Mathias <mathias.helsengren@hotmail.com>
2023-03-15 15:50:03 +0100

    Frontend arbejde

---
+++ Frontend/CodeAcademy/Whale talk/WhaleTalk.js
@@ -0,0 +1,7 @@
+const input = 'It was a slippery slope and he was willing to slide all the way to the deepest depths.';
+
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+const resultArray = [];
+
+for 
