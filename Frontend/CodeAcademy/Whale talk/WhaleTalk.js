e4307ddc3352f85750935766bfdcabccd5d5b9c0
Mathias <mathias.helsengren@hotmail.com>
2023-03-16 10:42:13 +0100

    whaletalk

--- Frontend/CodeAcademy/Whale talk/WhaleTalk.js
+++ Frontend/CodeAcademy/Whale talk/WhaleTalk.js
@@ -1,7 +1,34 @@
-const input = 'It was a slippery slope and he was willing to slide all the way to the deepest depths.';
+const input = 'turpentine and turtles';
 
 const vowels = ['a', 'e', 'i', 'o', 'u']
 
 const resultArray = [];
 
-for 
+for (let i = 0; i < input.length; i++) {
+    if (input[i] === 'e') {
+        resultArray.push(input[i]);
+    };
+    if (input[i] === 'u') {
+        resultArray.push(input[i]);
+    };
+
+    for (let j = 0; j < vowels.length; j++) {
+        if (input[i] === vowels[j]) {
+            resultArray.push(input[i]);
+        };
+
+    };
+};
+
+console.log(resultArray);
+
+const resultString = resultArray.join('').toUpperCase();
+
+console.log(resultString);
+
+
+if ('UUEEIEEAUUEE' === resultString) {
+    console.log('Its right')
+    } else {
+        console.log('Its wrong')
+    };
